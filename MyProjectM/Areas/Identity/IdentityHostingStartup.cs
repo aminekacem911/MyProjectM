39836eee940207f4ae66cd05fc5cdef91c200fa9
zorgan <zorgan@DESKTOP-U5IS5V7>
2022-01-15 13:10:20 -0800

    Add project files.

---
+++ MyProjectM/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyProjectM.Areas.Identity.Data;
+using MyProjectM.Data;
+
+[assembly: HostingStartup(typeof(MyProjectM.Areas.Identity.IdentityHostingStartup))]
+namespace MyProjectM.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthContextConnection")));
+
+                services.AddDefaultIdentity<MyProjectMUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AuthContext>();
+            });
+        }
+    }
+}
