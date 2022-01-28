f80bc2531639a4bb83605c2522a4245afad564d0
zorgan <amine.kacem1337@gmail.com>
2022-01-28 14:55:48 -0800

    start booking

---
+++ MyProjectM/Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyProjectM.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("~/Views/Home/Booking.cshtml");
+        }
+    }
+}
