7d58233b4c33074576e6b28f3021e098e5c25826
zorgan <amine.kacem1337@gmail.com>
2022-02-03 15:11:43 -0800

    perhaps

--- MyProjectM/Controllers/TicketsController.cs
+++ MyProjectM/Controllers/TicketsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,7 +11,7 @@ using MyProjectM.Models;
 
 namespace MyProjectM.Controllers
 {
-    
+    [Authorize]
     //[Route("admin/[controller]")]
     public class TicketsController : Controller
     {
