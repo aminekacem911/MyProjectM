456c29e4446bef06a8508f2cc767138739b9af6d
zorgan <amine.kacem1337@gmail.com>
2022-02-06 13:38:06 -0800

    fix migrations

---
+++ MyProjectM/Migrations/20220115223602_field-isadmin.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyProjectM.Migrations
+{
+    public partial class fieldisadmin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Isadmin",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Isadmin",
+                table: "AspNetUsers");
+        }
+    }
+}
